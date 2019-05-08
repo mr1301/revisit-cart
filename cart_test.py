38eb05db6b140ce56bed7e2d42280e4b535fbb9c
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-05-07 22:29:10 -0400

    test rounding

--- cart_test.py
+++ cart_test.py
@@ -3,8 +3,15 @@ from cart import to_usd
 
 def test_to_usd():
     result = to_usd(5)
-    assert result == "$5.00" #USD
+    assert result == "$5.00" # test USD sign
 
     result = to_usd(5.5)
-    assert result == "$5.50" #dec places
+    assert result == "$5.50" # test dec places
+
+    result = to_usd(5.555444)
+    assert result == "$5.56" #test rounding
+
+
+
+
  
