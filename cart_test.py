562d605868df2d7a9c17b1c18e9264cca50f7d00
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-05-07 22:52:23 -0400

    test match product keys

--- cart_test.py
+++ cart_test.py
@@ -1,5 +1,5 @@
 import pytest
-from cart import to_usd, tax_rate
+from cart import to_usd, tax_rate, find_product
 
 def test_to_usd():
     result = to_usd(5)
@@ -14,5 +14,11 @@ def test_to_usd():
 def test_tax_rate():
     assert(tax_rate) == 0.06
 
-
- 
+def test_find_product():
+   products = [
+        {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+        {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+        {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+    ]
+   match_product = find_product("2",products)
+   assert match_product["department"] == "pantry"
